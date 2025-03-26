8a5e64f0a45876022ed2b6ca870a9ef9cd58f314
piskova <v.piskova@beetroot.se>
2025-03-26 16:32:28 +0200

    Header menu: style currently active link

---
+++ components/main-header/nav-link.js
@@ -0,0 +1,20 @@
+"use client";
+import { usePathname } from "next/navigation";
+import classes from "./nav-link.module.css";
+import Link from "next/link";
+
+export default function NavLink({ href, children }) {
+  const path = usePathname();
+  return (
+    <Link
+      className={
+        path.startsWith(href)
+          ? `${classes.link} ${classes.active}`
+          : `${classes.link}`
+      }
+      href={href}
+    >
+      {children}
+    </Link>
+  );
+}
