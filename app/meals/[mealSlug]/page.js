6ff35e30bbca285c576414fc81065faebc4d7006
piskova <v.piskova@beetroot.se>
2025-03-27 17:21:30 +0200

    Meals page: get and display meals data

--- app/meals/[mealSlug]/page.js
+++ app/meals/[mealSlug]/page.js
@@ -1,11 +1,17 @@
-import React from 'react';
+import React from "react";
+import classes from "./page.module.css";
 
 const MealDetails = () => {
-    return (
-        <div>
-            MealDetails
-        </div>
-    );
+  return (
+    <>
+      <header className={classes.header}>
+        <h1>
+          Details
+        </h1>
+      </header>
+      <main className={classes.main}></main>
+    </>
+  );
 };
 
 export default MealDetails;
--- app/meals/page.js
+++ app/meals/page.js
@@ -1,11 +1,27 @@
-import React from 'react';
+import React from "react";
+import classes from "./page.module.css";
+import Link from "next/link";
+import MealsGrid from "../../components/meals/meals-grid";
+import { getMeals } from "@/lib/meals";
 
 const Meals = () => {
-    return (
-        <div>
-            <h1>Meals</h1>
-        </div>
-    );
+  const meals = getMeals();
+  return (
+    <>
+      <header className={classes.header}>
+        <h1>
+          Delicious meals <span className={classes.highlight}>for you</span>
+        </h1>
+        <p>Choose your recipe</p>
+        <p className={classes.cta}>
+          <Link href="/meals/share">Share your favourite recipe</Link>
+        </p>
+      </header>
+      <main className={classes.main}>
+        <MealsGrid meals={meals} />
+      </main>
+    </>
+  );
 };
 
 export default Meals;
