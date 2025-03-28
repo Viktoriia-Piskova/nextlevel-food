8890a0712c0325ead094505cfdb533d35ce4d293
piskova <v.piskova@beetroot.se>
2025-03-28 13:14:51 +0200

    General: add 404 page

---
+++ app/not-found.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const NotFound = () => {
+  return (
+    <main className="not-found">
+      <h1>Not found</h1>
+      <p>Sorry, we could not find the requested page</p>
+    </main>
+  );
+};
+
+export default NotFound;
