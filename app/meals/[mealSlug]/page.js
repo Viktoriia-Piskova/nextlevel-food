5eaad32c79cf4e98ba7093b913544a18489b82f0
piskova <v.piskova@beetroot.se>
2025-04-07 13:40:44 +0300

    Meal details page: display data and handle errors

--- app/meals/[mealSlug]/page.js
+++ app/meals/[mealSlug]/page.js
@@ -1,15 +1,37 @@
 import React from "react";
 import classes from "./page.module.css";
+import Image from "next/image";
+import { getMeal } from "@/lib/meals";
+import { notFound } from "next/navigation";
 
-const MealDetails = () => {
+const MealDetails = ({ params }) => {
+  const slug = params.mealSlug;
+  const meal = getMeal(slug);
+
+  if (!meal) {
+    notFound();
+  }
+  meal.instructions = meal.instructions.replace(/\n/g, "<br />");
   return (
     <>
       <header className={classes.header}>
-        <h1>
-          Details
-        </h1>
+        <div className={classes.image}>
+          <Image src={meal.image} alt={meal.title} fill />
+        </div>
+        <div className={classes.headerText}>
+          <h1>{meal.title}</h1>
+          <p className={classes.creator}>
+            by <a href={`mailto:${meal.creator_email}`}>{meal.creator}</a>
+          </p>
+          <p className={classes.summary}>{meal.summary}</p>
+        </div>
       </header>
-      <main className={classes.main}></main>
+      <main className={classes.main}>
+        <p
+          className={classes.instructions}
+          dangerouslySetInnerHTML={{ __html: meal.instructions }}
+        ></p>
+      </main>
     </>
   );
 };
